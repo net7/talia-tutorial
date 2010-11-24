b533fbaa3e6130aa17561760b3e5e836fe68133c
rik <giomi@netseven.it>
2010-11-24 16:41:16 +0100

    * Updated version of dyno LOD querying added, disabled for the current tutorial. * Some fixes to configure script for the new tutorial.

--- public/dyno/javascripts/main.js
+++ public/dyno/javascripts/main.js
@@ -28,10 +28,39 @@ $(function() {
             if(!$(a).attr("id")) settings.uri.push($(a).attr("href"));
         });
 
-        if(settings.uri.length && $("section#dyno-attributes")) {
-            $("section#dyno-attributes").dyno(settings);
+        if(settings.uri.length && $("#dyno-attributes")) {
+            $("#dyno-attributes").dyno(settings);
             $(".dyno-predicate").live("click", function(e) {return false});
             $(".dyno-source").live("click", function(e) {return false});
         }
     }
 });
+
+$(document).ready(function() {
+	  singleResultSlide()
+	  sourcesSlide();
+});
+
+function sourcesSlide() {	
+	  $("#dyno-sources-list").hide();
+	  $("#dyno-sources-toggle-bar").click(function () { 
+        $(this).prev("#dyno-sources-list").slideToggle();
+        $(this).children("p").toggleClass("closed");
+    });
+}
+
+function singleResultSlide() {
+	  $(".dyno-name").live('click', function () { 
+        $(this).next(".dyno-value").slideToggle();
+        $(this).children("p").toggleClass("closed");
+    });
+}
+
+
+
+
+
+
+
+
+
